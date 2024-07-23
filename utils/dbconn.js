b817650fd69569eb7ebba747d2cb9d2b753a2cb3
Dinesh10731j <dinesh1122@gmail.com>
2024-07-23 17:10:17 +0545

    mongodb url is added

--- utils/dbconn.js
+++ utils/dbconn.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const DbConn = async () => {
+const DbConn = async  () => {
     try {
-        await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+     await mongoose.connect(process.env.MONGO_URI);
 
         console.log('Connected to MongoDB');
     } catch (err) {
