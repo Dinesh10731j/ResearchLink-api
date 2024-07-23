f6ebd84fda1aa58467f24ac1ef8c06920917666c
Dinesh10731j <dinesh1122@gmail.com>
2024-07-23 16:51:02 +0545

    changes done

--- utils/dbconn.js
+++ utils/dbconn.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const DbConn = async ()=>{
-    try{
-await mongoose.connect(process.env.DB_URI);
-console.log('Connection successful');
-    }catch(err){
-        console.log('Error while connecting the database',err);
+const DbConn = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err.message);
     }
-}
-
-
+};
 
 module.exports = DbConn;
