474eaaf73a48f193dab4f7380519cd34aa45917d
Dinesh10731j <dinesh1122@gmail.com>
2024-07-25 15:58:04 +0545

    error fixing

--- controllers/getresearchpaper.controller.js
+++ controllers/getresearchpaper.controller.js
@@ -1,19 +1,17 @@
-
 const UploadResearchPaperModel = require("../controllers/uploadpaper.controller");
-const GetResearchPaper = async (req,res)=>{
-    try{
+
+const GetResearchPaper = async (req, res) => {
+    try {
         const ResearchPapers = await UploadResearchPaperModel.find({});
-        if(ResearchPapers.length === 0){
-            return res.status(404).json({msg:"Research papers not found",success:true})
+        if (ResearchPapers.length === 0) {
+            return res.status(404).json({ msg: "Research papers not found", success: false });
         }
 
+        return res.status(200).json({ msg: "Research paper fetch successfully", success: true, data: ResearchPapers });
 
-        return res.status(200).json({msg:"Rsearch paper fetch successfully",success:true})
-
-    }catch(err){
-        res.status(500).json({msg:'Internal server error',success:false},err)
+    } catch (err) {
+        res.status(500).json({ msg: 'Internal server error', success: false, error: err.message });
     }
 }
-
 
 module.exports = GetResearchPaper;
