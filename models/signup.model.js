b249cf3d6b4ca100f624c95fdf2ac7c84fd15da6
Dinesh10731j <dinesh1122@gmail.com>
2024-07-25 21:04:48 +0545

    array removed

--- models/signup.model.js
+++ models/signup.model.js
@@ -23,7 +23,7 @@ const UserSchema = new Schema({
   },
 
 
-  profile:[{ type: Schema.Types.ObjectId,ref:'Userprofile' }],
+  profile:{ type: Schema.Types.ObjectId,ref:'Userprofile' },
    
   
 });
