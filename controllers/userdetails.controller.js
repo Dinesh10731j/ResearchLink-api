35533fd1b3112600c3119502fd7e91386b2c03a2
Dinesh10731j <dinesh1122@gmail.com>
2024-07-26 15:21:43 +0545

    reference error fixing

--- controllers/userdetails.controller.js
+++ controllers/userdetails.controller.js
@@ -1,14 +1,19 @@
+const UserModel = require("../models/signup.model");
+const Userdetails = async (req, res) => {
+  const { userid } = req.params;
+  try {
+    // Populate the profile field in the user details
+    const userdetails = await UserModel.findById(userid).populate('profile');
+    console.log(userdetails)
 
-const UserModel = require("../models/signup.model")
-const Userdetails = async (req,res)=>{
-    const {userid} = req.params;
-    try{
-const userdetails = await UserModel.findById(userid);
-res.status(200).json({msg:'User detai',success:true,data:userdetails});
-    }catch(err){
-res.status(500).json({msg:'Internal server error',success:false},err)
+    if (!userdetails) {
+      return res.status(404).json({ msg: 'User not found', success: false });
     }
-}
 
+    res.status(200).json({ msg: 'User details fetched successfully', success: true, data: userdetails });
+  } catch (err) {
+    res.status(500).json({ msg: 'Internal server error', success: false, error: err.message });
+  }
+};
 
 module.exports = Userdetails;
