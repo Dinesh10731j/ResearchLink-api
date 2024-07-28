60031da3ae44f9c816d880475ab1159dc526841a
Dinesh10731j <dinesh1122@gmail.com>
2024-07-28 17:32:35 +0545

    userId is added in req.body

--- controllers/uploadpaper.controller.js
+++ controllers/uploadpaper.controller.js
@@ -1,7 +1,7 @@
 const UploadResearchPaperModel = require("../models/uploadpaper.model");
 const UploadPaper = async (req, res) => {
   try {
-    const { title, description, file } = req.body;
+    const { title, description, file} = req.body;
     await UploadResearchPaperModel.create({
       title: title,
       description: description,
