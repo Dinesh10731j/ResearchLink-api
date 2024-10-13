8753d781620d857307493c3922d149b93528ed4b
Dinesh10731j <dinesh1122@gmail.com>
2024-10-13 10:52:47 +0545

    changes in dislike controller

--- controllers/dislikes.controller.js
+++ controllers/dislikes.controller.js
@@ -1,35 +1,37 @@
+const mongoose = require('mongoose');
 const UploadResearchPaperModel = require('../models/uploadpaper.model');
 
 const Dislikes = async (req, res) => {
   try {
     const { paperId, userId } = req.body;
 
-
     if (!paperId || !userId) {
       return res.status(400).json({ msg: 'Missing paperId or userId' });
     }
 
-   
+    
+    const userObjectId = mongoose.Types.ObjectId(userId);
+
+
     const paper = await UploadResearchPaperModel.findById(paperId);
     if (!paper) {
       return res.status(404).json({ msg: 'Paper not found' });
     }
-    const userObjectId = mongoose.Types.ObjectId(userId);
-   
+
     if (!Array.isArray(paper.dislikeCount)) {
       paper.dislikeCount = [];
     }
 
-    const isDisliked = paper.dislikeCount.includes(userObjectId);
+    const isDisliked = paper.dislikeCount.some(id => id.equals(userObjectId));
 
-  
+ 
     if (isDisliked) {
-      paper.dislikeCount = paper.dislikeCount.filter(id => id !== userId);
+      paper.dislikeCount = paper.dislikeCount.filter(id => !id.equals(userObjectId));
     } else {
-      paper.dislikeCount.push(userId);
+      paper.dislikeCount.push(userObjectId);
     }
 
-   
+    
     await paper.save();
 
     return res.status(200).json({
