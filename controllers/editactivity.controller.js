5f56c55263f4e9344cc0c9c668a683d0e81e22c2
Dinesh10731j <dinesh1122@gmail.com>
2024-10-19 19:24:02 +0545

    minor changes done in editactivity controller

--- controllers/editactivity.controller.js
+++ controllers/editactivity.controller.js
@@ -31,7 +31,7 @@ const editActivity = async (req, res) => {
     return res.status(200).json({
       msg: "Activity edited successfully",
       success: true,
-      data: editedData,
+     
     });
 
   } catch (err) {
