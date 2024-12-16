68a9350b0d3f9c442e8b2783a13c8ee4ea754e2c
Dinesh10731j <dinesh1122@gmail.com>
2024-12-16 17:27:49 +0545

    like and dislike endpoint issue fixed

--- controllers/dislikes.controller.js
+++ controllers/dislikes.controller.js
@@ -1,4 +1,3 @@
-
 const UploadResearchPaperModel = require('../models/uploadpaper.model');
 
 const Dislikes = async (req, res) => {
@@ -9,14 +8,14 @@ const Dislikes = async (req, res) => {
       return res.status(400).json({ msg: 'Missing paperId or userId' });
     }
 
-    
-  
-
+    console.log('Received paperId:', paperId);
+    console.log('Received userId:', userId);
 
     const paper = await UploadResearchPaperModel.findById(paperId);
     if (!paper) {
       return res.status(404).json({ msg: 'Paper not found' });
     }
+
 
     if (!Array.isArray(paper.dislikeCount)) {
       paper.dislikeCount = [];
@@ -24,15 +23,21 @@ const Dislikes = async (req, res) => {
 
     const isDisliked = paper.dislikeCount.includes(userId);
 
- 
+  
+
     if (isDisliked) {
-        paper.dislikeCount = paper.dislikeCount.filter(id => id !== userId)
+      paper.dislikeCount = paper.dislikeCount.filter(id => {
+    
+        return id.toString() !== userId.toString();
+      });
+    
     } else {
       paper.dislikeCount.push(userId);
+
     }
 
-    
     await paper.save();
+
 
     return res.status(200).json({
       msg: 'Dislike action successful',
@@ -40,6 +45,7 @@ const Dislikes = async (req, res) => {
     });
 
   } catch (err) {
+    console.error('Error:', err);
     return res.status(500).json({
       msg: 'Internal server error',
       success: false,
